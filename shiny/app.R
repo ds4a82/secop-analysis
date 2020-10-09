1b49e7ed48f995324440aa7a3d8c501e62fa0387
cecabrera <cecabrera55@gmail.com>
2020-10-09 14:01:04 -0500

    preubas

--- shiny/app.R
+++ shiny/app.R
@@ -40,10 +40,6 @@ server <- function(input, output) {
   set.seed(122)
   histdata <- rnorm(500)
   
-  output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
-  })
 }
 
 shinyApp(ui, server)
