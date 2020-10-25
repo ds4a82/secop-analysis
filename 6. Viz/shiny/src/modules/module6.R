101aa4c78b44fe165cfe24e62408ff4160a461fa
cecabrera <cecabrera55@gmail.com>
2020-10-24 19:55:35 -0500

    ajustes en video

--- 6. Viz/shiny/src/modules/module6.R
+++ 6. Viz/shiny/src/modules/module6.R
@@ -6,7 +6,10 @@ module6UI <- function(id){
     , br()
     , fluidRow(
        ""
-        , HTML('<div style="overflow:hidden;position: relative;"><iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="104" height="443" type="text/html" src="https://www.youtube.com/embed/v_3NCu6v4ew?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0"></iframe><div style="position: absolute;bottom: 10px;left: 0;right: 0;margin-left: auto;margin-right: auto;color: #000;text-align: center;"><small style="line-height: 1.8;font-size: 0px;background: #fff;"> <a href="https://deloge.de/" rel="nofollow">Deloge</a> </small></div><style>.newst{position:relative;text-align:right;height:900px;width:100%;} #gmap_canvas img{max-width:none!important;background:none!important}</style></div><br />')
+       , column(
+         HTML('<div style="overflow:hidden;position: relative;"><iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0"width="100%" height="600px" type="text/html" src="https://www.youtube.com/embed/v_3NCu6v4ew?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0"></iframe><div style="position: absolute;bottom: 10px;left: 0;right: 0;margin-left: auto;margin-right: auto;color: #000;text-align: center;"><small style="line-height: 1.8;font-size: 0px;background: #fff;"> <a href="https://deloge.de/" rel="nofollow">Deloge</a> </small></div><style>.newst{position:relative;text-align:right;height:900px;width:100%;} #gmap_canvas img{max-width:none!important;background:none!important}</style></div><br />')
+         , width = 12
+        )
      )
   )
   tagList(main)
