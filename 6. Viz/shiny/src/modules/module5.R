e18b84dfb97c93e72a03441349b6f4b60f275876
cecabrera <cecabrera55@gmail.com>
2020-11-09 08:48:39 -0500

    reorganización

--- 6. Viz/shiny/src/modules/module5.R
+++ 6. Viz/shiny/src/modules/module5.R
@@ -7,9 +7,7 @@ module5UI <- function(id){
      , fluidRow(
        ""
        , column(width = 2)
-       , column(width = 8
-                , HTML('<iframe width="100%" height="500" src="https://www.mintic.gov.co/portal/604/articles-145965_recurso_1.pdf" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
-       )
+       , column(width = 8)
        , column(width = 2)
      )
   )
