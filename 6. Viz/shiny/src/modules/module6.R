5b37aad497d3e2c870f7cd0ce9072dbff63f4b76
unknown <cindylrr@gmail.com>
2020-11-10 07:43:08 -0500

    Change background color

--- 6. Viz/shiny/src/modules/module6.R
+++ 6. Viz/shiny/src/modules/module6.R
@@ -18,48 +18,48 @@ module6UI <- function(id){
     # Team
     , fluidRow(
       ""
-      , column(wellPanel(
-        img(src = "https://github.com/ds4a82/secop-analysis/raw/master/6.%20Viz/shiny/img/team/camilo_photo.jpg", style="width:150px;height:150px;display:block;margin-left:auto;margin-right:auto;border-radius:75px;border:4px solid #171738;") 
-        , h2("Camilo Cabrera, MSc", style="text-align:center")
-        , p("Data Science Consultant at fidelio.com.co", style="text-align:center")
+      , column(wellPanel(style = "background: #171738",
+        img(src = "https://github.com/ds4a82/secop-analysis/raw/master/6.%20Viz/shiny/img/team/camilo_photo.jpg", style="width:150px;height:150px;display:block;margin-left:auto;margin-right:auto;border-radius:75px;border:4px solid #fcfcfc;") 
+        , h2("Camilo Cabrera, MSc", style="color:white;text-align:center")
+        , p("Data Science Consultant at fidelio.com.co", style="color:white;text-align:center")
         , a(icon(name = "envelope", "fa-3x"), href = "mailto:ccabrera@fidelio.com.co", style="display:inline-block;margin-left:110px;text-align:center;")
         , a(icon(name = "linkedin", "fa-3x"), href = "https://www.linkedin.com/in/camilo-cabrera/", style="display:inline-block;margin-right:90px;text-align:center;")
       ), width = 4)
-      , column(wellPanel(
-        img(src = "https://github.com/ds4a82/secop-analysis/raw/master/6.%20Viz/shiny/img/team/cindy_photo.jpg", style="width:150px;height:150px;display:block;margin-left:auto;margin-right:auto;border-radius:75px;border:4px solid #171738;")
-        , h2("Cindy Ramirez", style="text-align:center")
-        , p("Business Intelligence Senior at Rappi", style="text-align:center")
+      , column(wellPanel(style = "background: #171738",
+        img(src = "https://github.com/ds4a82/secop-analysis/raw/master/6.%20Viz/shiny/img/team/cindy_photo.jpg", style="width:150px;height:150px;display:block;margin-left:auto;margin-right:auto;border-radius:75px;border:4px solid #fcfcfc;")
+        , h2("Cindy Ramirez", style="color:white;text-align:center")
+        , p("Business Intelligence Senior at Rappi", style="color:white;text-align:center")
         , a(icon(name = "envelope", "fa-3x"), href = "mailto:cindylrr@gmail.com", style="display:inline-block;margin-left:110px;text-align:center;")
         , a(icon(name = "linkedin", "fa-3x"), href = "https://www.linkedin.com/in/cindy-ramirez-restrepo", style="display:inline-block;margin-right:90px;text-align:center;")
       ), width = 4)
-      , column(wellPanel(
-        img(src = "https://github.com/ds4a82/secop-analysis/raw/master/6.%20Viz/shiny/img/team/jorge_photo.jpg", style="width:150px;height:150px;display:block;margin-left:auto;margin-right:auto;border-radius:75px;border:4px solid #171738;")
-        , h2("Jorge Enciso", style="text-align:center")
-        , p("Business Intelligence Analyst at RobinFood", style="text-align:center")
+      , column(wellPanel(style = "background: #171738",
+        img(src = "https://github.com/ds4a82/secop-analysis/raw/master/6.%20Viz/shiny/img/team/jorge_photo.jpg", style="width:150px;height:150px;display:block;margin-left:auto;margin-right:auto;border-radius:75px;border:4px solid #fcfcfc;")
+        , h2("Jorge Enciso", style="color:white;text-align:center")
+        , p("Business Intelligence Analyst at RobinFood", style="color:white;text-align:center")
         , a(icon(name = "envelope", "fa-3x"), href = "mailto:jorgeeduardo.enciso@gmail.com", style="display:inline-block;margin-left:110px;text-align:center;")
         , a(icon(name = "linkedin", "fa-3x"), href = "https://www.linkedin.com/in/jorge-eduardo-enciso-agudelo-6b01b4116/", style="display:inline-block;margin-right:90px;text-align:center;")
       ), width = 4)
     )
     , fluidRow(
       ""
-      , column(wellPanel(
-        img(src = "https://github.com/ds4a82/secop-analysis/raw/master/6.%20Viz/shiny/img/team/karina_photo.jpg", style="width:150px;height:150px;display:block;margin-left:auto;margin-right:auto;border-radius:75px;border:4px solid #171738;")
-        , h2("Karina Mesa", style="text-align:center")
-        , p("Business Analytics Lead. Financial Services Industry", style="text-align:center")
+      , column(wellPanel(style = "background: #171738",
+        img(src = "https://github.com/ds4a82/secop-analysis/raw/master/6.%20Viz/shiny/img/team/karina_photo.jpg", style="width:150px;height:150px;display:block;margin-left:auto;margin-right:auto;border-radius:75px;border:4px solid #fcfcfc;")
+        , h2("Karina Mesa", style="color:white;text-align:center")
+        , p("Business Analytics Lead. Financial Services Industry", style="color:white;text-align:center")
         , a(icon(name = "envelope", "fa-3x"), href = "mailto:karinamesa@gmail.com", style="display:inline-block;margin-left:110px;text-align:center;")
         , a(icon(name = "linkedin", "fa-3x"), href = "https://www.linkedin.com/in/karina-mesa-a376aa27/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3Bw5kaMvFCQAiGWNSGXWCpNA%3D%3D", style="display:inline-block;margin-right:90px;text-align:center;")
       ), width = 4)
-      , column(wellPanel(
-        img(src = "https://github.com/ds4a82/secop-analysis/raw/master/6.%20Viz/shiny/img/team/nicolas_photo.jpg", style="width:150px;height:150px;display:block;margin-left:auto;margin-right:auto;border-radius:75px;border:4px solid #171738;")
-        , h2("Nicolas Casas", style="text-align:center")
-        , p("FP&A Specialist at Alianza Fiduciaria", style="text-align:center")
+      , column(wellPanel(style = "background: #171738",
+        img(src = "https://github.com/ds4a82/secop-analysis/raw/master/6.%20Viz/shiny/img/team/nicolas_photo.jpg", style="width:150px;height:150px;display:block;margin-left:auto;margin-right:auto;border-radius:75px;border:4px solid #fcfcfc;")
+        , h2("Nicolas Casas", style="color:white;text-align:center")
+        , p("FP&A Specialist at Alianza Fiduciaria", style="color:white;text-align:center")
         , a(icon(name = "envelope", "fa-3x"), href = "mailto:pncasasp@gmail.com", style="display:inline-block;margin-left:110px;text-align:center;")
         , a(icon(name = "linkedin", "fa-3x"), href = "https://www.linkedin.com/in/pedro-nicolas-casas", style="display:inline-block;margin-right:90px;text-align:center;")
       ), width = 4)
-      , column(wellPanel(
-        img(src = "https://github.com/ds4a82/secop-analysis/raw/master/6.%20Viz/shiny/img/team/samuel_photo.jpg", style="width:150px;height:150px;display:block;margin-left:auto;margin-right:auto;border-radius:75px;border:4px solid #171738;")
-        , h2("Samuel Perez", style="text-align:center")
-        , p("Cadastral Engineer and Geodest & Economist", style="text-align:center")
+      , column(wellPanel(style = "background: #171738",
+        img(src = "https://github.com/ds4a82/secop-analysis/raw/master/6.%20Viz/shiny/img/team/samuel_photo.jpg", style="width:150px;height:150px;display:block;margin-left:auto;margin-right:auto;border-radius:75px;border:4px solid #fcfcfc;")
+        , h2("Samuel Perez", style="color:white;text-align:center")
+        , p("Cadastral Engineer and Geodest & Economist", style="color:white;text-align:center")
         , a(icon(name = "envelope", "fa-3x"), href = "mailto:saaperezca@unal.edu.co", style="display:inline-block;margin-left:110px;text-align:center;")
         , a(icon(name = "linkedin", "fa-3x"), href = "https://www.linkedin.com/in/samuel-perez-spatial-data-scientist/", style="display:inline-block;margin-right:90px;text-align:center;")
       ), width = 4)
@@ -67,10 +67,10 @@ module6UI <- function(id){
     , fluidRow(
       ""
       , column(width = 4)
-      , column(wellPanel(
-        img(src = "https://github.com/ds4a82/secop-analysis/blob/master/6.%20Viz/shiny/img/team/yasmin_photo.jpg?raw=true", style="width:150px;height:150px;display:block;margin-left:auto;margin-right:auto;border-radius:75px;border:4px solid #171738;")
-        , h2("Yasmin Moya Villa, MSc", style="text-align:center")
-        , p("PhD Student", style="text-align:center")
+      , column(wellPanel(style = "background: #171738",
+        img(src = "https://github.com/ds4a82/secop-analysis/blob/master/6.%20Viz/shiny/img/team/yasmin_photo.jpg?raw=true", style="width:150px;height:150px;display:block;margin-left:auto;margin-right:auto;border-radius:75px;border:4px solid #fcfcfc;")
+        , h2("Yasmin Moya Villa, MSc", style="color:white;text-align:center")
+        , p("PhD Student", style="color:white;text-align:center")
         , a(icon(name = "envelope", "fa-3x"), href = "mailto:ymoyav@unicartagena.edu.co", style="display:inline-block;margin-left:110px;text-align:center;")
         , a(icon(name = "linkedin", "fa-3x"), href = "www.linkedin.com/in/yasmin-moya-villa", style="display:inline-block;margin-right:90px;text-align:center;")
       ), width = 4)
