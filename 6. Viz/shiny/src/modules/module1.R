f8982abd53ef52e9c7589282269ffa6e20d8efa5
unknown <cindylrr@gmail.com>
2020-11-13 12:22:46 -0500

    Add img context

--- 6. Viz/shiny/src/modules/module1.R
+++ 6. Viz/shiny/src/modules/module1.R
@@ -6,6 +6,7 @@ module1UI <- function(id){
                , br()
                , wellPanel(
                  h1("Context")
+                 , img(src = "https://github.com/ds4a82/secop-analysis/blob/master/6.%20Viz/shiny/img/Context.png?raw=true", style="width:200px;height:200px;float: left;")
                  , fluidRow(p("Public procurement in Colombia is carried out by State entities that purchase goods and services necessary for its operation. The details of the contracts must be registered in the Electronic System for Public Procurement (SECOP). Until October 2020, in Colombia contracts for more than COP 20.3 billion have been signed through the transactional platform SECOP II . This significant amount of resources is managed by Colombia Compra Eficiente, the public procurement agency that develops and promotes public policies in order to achieve greater efficiency, transparency and optimization of public money."))
                )
                , br()
