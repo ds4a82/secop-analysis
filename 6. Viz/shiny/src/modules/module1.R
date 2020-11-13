dc01ed04e263b58ac0acc05b1dd9ae40ee916fec
unknown <cindylrr@gmail.com>
2020-11-13 12:00:14 -0500

    Change solution

--- 6. Viz/shiny/src/modules/module1.R
+++ 6. Viz/shiny/src/modules/module1.R
@@ -11,18 +11,14 @@ module1UI <- function(id){
                , br()
                , wellPanel(
                  h1("Problem")
-                 , fluidRow(p("Due to the large number of contracts registered on the SECOP platforms, Colombia Compra Eficiente requires group contracts that are related by identifying common words in the contractual object to generate insights, optimize the public procurement process, and finally help public entities to improve purchasing. However, this grouping is done manually, involving time and effort that could be used in more relevant activities."))
+                 , fluidRow(p("Due to the large number of contracts registered on the SECOP platforms (+10 million), Colombia Compra Eficiente requires to group contracts by identifying common words in the contractual object to generate insights, optimize the public procurement process, and finally help public entities to improve purchasing. However, this grouping is done manually, involving time and effort that could be used in more relevant activities."))
                )
                , br()
                , wellPanel(
                  h1("Solution")
-                 , fluidRow(p("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."))
-               )
-               , br()
-               , wellPanel(
-                 h1("Methodology")
-                 , fluidRow(p("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."))
-               )
+                 , fluidRow(p("We have developed this application to provide a tool to Colombia Compra Efficient and the general public that allows them to achieve a greater understanding of the goods and services that public entities are buying. To build the app we follow this steps:")
+                 , img(src = "https://github.com/ds4a82/secop-analysis/blob/master/6.%20Viz/shiny/img/Diagram.png?raw=true", style="width:600px;height:400px;display:block;margin-left:auto;margin-right:auto;")
+               ))
                , br()
   )
   tagList(main)
