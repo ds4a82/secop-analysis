fa9101b68dff47ca56c320ca86ed4ca890565f68
cecabrera <cecabrera55@gmail.com>
2020-11-13 10:20:42 -0500

    actualización de texto de siguientes pasos

--- 6. Viz/shiny/src/modules/module5.R
+++ 6. Viz/shiny/src/modules/module5.R
@@ -1,25 +1,32 @@
 module5UI <- function(id){
   ns <- NS(id)
-  main <- list(""
-               , br()
-               , wellPanel(
-                 h1("Database integration")
-                 , fluidRow(
-                   p("Everytime the dashboard runs it is loading a local '.RDS' file containing 100.000 aleatory contracts read from a PostgreSQL database on an AWS server. The next step regarding databases, is to take the dashboards command controls and plug them directly into the database to reduce loading time and open the possibility to update the database on a dialy, weekly or monthly basis. ")
-                   , br()
-                   , p("This approach opens up the opportunity to schedule a dialy, weekly or monthly download of the files in Datos Abiertos (contracts main location), process them locally (data cleaning) and upload them into the AWS database. Based on the frecuency of the schedule, the dashboard could be updated in real time.")
-                  )
-               )
-               , br()
-               , wellPanel(
-                 h1("Contracts manual categorization")
-                 , fluidRow(
-                   p("The are several main categories already identified by the Colombia Compra Eficiente (CCE) team. Some of them involve \"health\", \"construction\", \"infraestructure\" and \"technology\" among many others. The next step would be to semi-automatically categorize all the contracts based on the keywords in their description. For example by matching the word \"hospital\" to the Health category. ")
-                   , br()
-                   , p("After processing all the categories, the Clustering and LDA topic modelling analysis can be zoomed-in into each category to analize their content.")
-                  )
-               )
-               , br()
+  main <- list(
+    ""
+    , br()
+    , fluidRow(
+      column(
+        width = 6
+        , wellPanel(
+         h2("1. Database integration")
+         , fluidRow(
+           p("Everytime the dashboard runs, it loads a local '.RDS' file containing 100.000 aleatory contracts read from a PostgreSQL database on an AWS server. The next step regarding databases, is to take the dashboards command controls and plug them directly into the database to reduce loading time.")
+           , br()
+           , p("This approach opens up the opportunity to schedule a dialy, weekly or monthly download of the files in datos.gov.co (contracts main location), process them locally (data cleaning) and upload them into the AWS database. Based on the frecuency of the schedule defined, the dashboard could be updated in real time.")
+          )
+        )
+      )
+      , column(
+        width = 6
+        , wellPanel(
+         h2("2. Contracts manual categorization")
+         , fluidRow(
+           p("The are several main categories already identified by the Colombia Compra Eficiente (CCE) team. Some of them involve \"health\", \"construction\", \"infraestructure\" and \"technology\" among many others. The next step would be to list the most relevant categories and semi-automatically categorize all the contracts based on the keywords in their description. For example the word \"hospital\" can be matched with the Health category.")
+           , br()
+           , p("After processing all the categories, Clustering and LDA topic modelling analysis can be re-run into each category to analize their main topics.")
+          )
+        )
+      )
+    )
   )
   tagList(main)
 }
