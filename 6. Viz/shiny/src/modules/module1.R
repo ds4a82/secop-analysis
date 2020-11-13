caf832d5f6c9dcf20c2a0f2e214c61010a398a53
unknown <cindylrr@gmail.com>
2020-11-13 08:52:09 -0500

    Change Context and Solution

--- 6. Viz/shiny/src/modules/module1.R
+++ 6. Viz/shiny/src/modules/module1.R
@@ -6,12 +6,12 @@ module1UI <- function(id){
                , br()
                , wellPanel(
                  h1("Context")
-                 , fluidRow(p("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."))
+                 , fluidRow(p("Public procurement in Colombia is carried out by State entities that purchase goods and services necessary for its operation. The details of the contracts must be registered in the Electronic System for Public Procurement (SECOP). Until October 2020, in Colombia contracts for more than COP 20.3 billion have been signed through the transactional platform SECOP II . This significant amount of resources is managed by Colombia Compra Eficiente, the public procurement agency that develops and promotes public policies in order to achieve greater efficiency, transparency and optimization of public money."))
                )
                , br()
                , wellPanel(
                  h1("Problem")
-                 , fluidRow(p("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."))
+                 , fluidRow(p("Due to the large number of contracts registered on the SECOP platforms, Colombia Compra Eficiente requires group contracts that are related by identifying common words in the contractual object to generate insights, optimize the public procurement process, and finally help public entities to improve purchasing. However, this grouping is done manually, involving time and effort that could be used in more relevant activities."))
                )
                , br()
                , wellPanel(
