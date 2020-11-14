ff87819e43f243ca454504e8289d115ec9c0c7d2
unknown <cindylrr@gmail.com>
2020-11-14 11:13:20 -0500

    Add white paper

--- 6. Viz/shiny/src/modules/module5.R
+++ 6. Viz/shiny/src/modules/module5.R
@@ -27,6 +27,18 @@ module5UI <- function(id){
         )
       )
     )
+    , br()
+    , h1("White paper", style = "text-align: center;")
+    , br()
+    # Document
+    , fluidRow(
+      ""
+      , column(width = 2)
+      , column(width = 8
+               , HTML('<iframe width="100%" height="500" src="https://www.mintic.gov.co/portal/604/articles-145965_recurso_1.pdf" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+      )
+      , column(width = 2)
+    )  
   )
   tagList(main)
 }
