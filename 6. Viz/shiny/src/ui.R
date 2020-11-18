f05bb8d96ea785ac8bd0e83c5b8705a7992e4d27
cecabrera <cecabrera55@gmail.com>
2020-11-18 09:33:38 -0500

    tags de analytics

--- 6. Viz/shiny/src/ui.R
+++ 6. Viz/shiny/src/ui.R
@@ -12,7 +12,30 @@ ui <- function(){shinyUI(dashboardPage(title = 'Secop Topics',
   , body = dashboardBody(
     shinyjs::useShinyjs()
     , fluidPage(
-      disconnectMessage( # In case de app disconnects
+      tags$head( # Google Tag Manager
+        HTML( 
+          "
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N7QGLVF');</script>
+<!-- End Google Tag Manager -->
+
+          "
+        )
+      )
+      , tags$body(
+      HTML('
+                  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N7QGLVF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+                  ')
+      )
+      , disconnectMessage( # In case de app disconnects
         text = "Your session has timed out.",
         refresh = "Refresh",
         background = "#404040E6",
